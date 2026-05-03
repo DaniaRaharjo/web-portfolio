e50320f5ec1b92a396be622f5f1074ad0538f25d
Dania <daniakraharjo@gmail.com>
2026-05-03 17:52:11 +0800

    Controller, policy, route

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,30 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProjectsController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/projects/{project:slug}', [ProjectsController::class, 'show'])->name('projects.show');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route:prefix('admin')->group(function(){
+        Route::post('/projects', [ProjectsController::class, 'store'])->name('projects.store');
+        Route::get('/projects/create', [ProjectsController::class, 'create'])->name('projects.create');
+        Route::get('/projects/{project:slug}/edit', [ProjectsController::class, 'edit'])->name('projects.edit');
+        Route::put('/projects/{project:slug}', [ProjectsController::class, 'update'])->name('projects.update');
+        Route::delete('/projects/{project:slug}', [ProjectsController::class, 'destroy'])->name('projects.destroy');
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/projects', [Project])
 
 require __DIR__.'/auth.php';
